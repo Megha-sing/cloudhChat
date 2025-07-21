b596a73a959d10ff23a0fa4ee633d7b35a554fba
Megha Singh <meghasingh2k24@gmail.com>
2025-07-21 22:21:42 +0530

    error solved

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,22 +5,32 @@ import dotenv from 'dotenv';
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import {app, server} from './lib/socket.js';
+import { app, server } from './lib/socket.js';
 import path from 'path';
+
 dotenv.config();
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
+
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5174",
+  "http://127.0.0.1:5173",
+  "http://127.0.0.1:5174",
+];
+
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || origin.startsWith("http://localhost")) {
-      return callback(null, true);
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
     }
-    callback(new Error("Not allowed by CORS"));
   },
   credentials: true,
 }));
-
 
 app.use(cookieParser());
 app.use(express.json({ limit: "100mb" }));
@@ -28,12 +38,15 @@ app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-if(process.env.NODE_ENV === "production") {
+
+
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
   });
 }
+
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
