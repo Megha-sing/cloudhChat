fdb9375213a54c44a1e32876e72edbcf0d38de87
Megha Singh <meghasingh2k24@gmail.com>
2025-07-21 21:59:32 +0530

    ready to deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,10 +6,11 @@ import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import {app, server} from './lib/socket.js';
+import path from 'path';
 dotenv.config();
 
 const PORT = process.env.PORT;
-
+const __dirname = path.resolve();
 app.use(cors({
   origin: (origin, callback) => {
     if (!origin || origin.startsWith("http://localhost")) {
@@ -27,7 +28,12 @@ app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+  });
+}
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
